46edf525fe51c2d047927f072b3ea9b95b6ade23
prosales95 <pablo_25rrs@hotmail.com>
2018-05-19 06:35:43 +0200

    <commit message>

---
+++ src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {RegisterPage} from '../register/register';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+
+@Component({
+	selector: 'page-home',
+	templateUrl: 'home.html'
+})
+export class HomePage {
+	public inputVal:string;
+	username:string;
+	password:string;
+	data: Observable<any>;
+	result: any =[];
+	constructor(public navCtrl: NavController, public http:HttpClient) {
+
+
+	}
+
+	buttonClick(){
+		console.log("Clicked");
+		alert(this.inputVal + "I see you are obedient. I like that.")
+	}
+
+	goRegister(){
+		this.navCtrl.push(RegisterPage)
+
+	}
+
+	login(){
+		console.log ("Username " + this.username);
+		console.log("Password "+this.password);
+	}
+
+	getData() {
+		var url ='https://jsonplaceholder.typicode.com/posts/1' ;
+		this.data =this.http.get(url);
+		this.data.subscribe (data => {
+			console.log(data);
+			this.result=data;
+		})
+		console.log('getData executed');
+	}
+
+}
---
+++ src/pages/register/register.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+ @IonicPage()
+ @Component({
+ 	selector: 'page-register',
+ 	templateUrl: 'register.html',
+ })
+ export class RegisterPage {
+ 	username:string;
+ 	password:string;
+ 	repassword:string;
+
+ 	constructor(public navCtrl: NavController, public navParams: NavParams) {
+ 	}
+
+ 	ionViewDidLoad() {
+ 		console.log('ionViewDidLoad RegisterPage');
+ 	}
+
+ 	register(){
+ 		if (this.username.length ==0 ||
+ 			this.password.length == 0 || this.repassword.length==0){
+ 			alert("Comon dude please fill all fields")	
+ 	}
+ }
+}
